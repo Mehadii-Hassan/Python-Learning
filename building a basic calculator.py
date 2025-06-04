b0c3121bdb1bd8f475c11c1e29639dee6cddb691
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-04 23:50:16 +0600

    building a basic calculator

---
+++ building a basic calculator.py
@@ -0,0 +1,14 @@
+num1 = input("Enter first number : ")
+num2 = input("Enter second number : ")
+
+sum = int(num1) + int(num2)
+sub = int(num1) - int(num2)
+mult = int(num1) * int(num2)
+div = int(num1) / int(num2)
+mod = int(num1) % int(num2)
+
+print("Your sum is : " + str(sum))
+print("Your sub is : " + str(sub))
+print("Your mult is : " + str(mult))
+print("Your div is : " + str(div))
+print("Your mod is : " + str(mod))
