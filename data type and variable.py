a4b6bcab6b67bcf42f28b05b6b97082b391250e6
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-04 21:52:24 +0600

    Data type and variable

---
+++ data type and variable.py
@@ -0,0 +1,8 @@
+character_name = "Mehedi"
+character_age = "22"
+print("There once was a man named " + character_name + ".")
+print("He was " + character_age + " years old.")
+
+character_name = "Hassan"
+print("He really liked the name " + character_name + ".")
+print("But he didn't like being " + character_age + ".")
