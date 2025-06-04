4b085f8a1f8857f3ecdcf2333ae4271496e1487a
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-04 23:10:23 +0600

    working with numbers

---
+++ working with number.py
@@ -0,0 +1,34 @@
+print(2) #output will be 2
+print(2.9301) #output will be 2.9301
+print(-2.9301) #output will be -2.9301
+
+print(3 + 4) #output will be 7
+print(3 + 4.5) #output will be 7.5
+print(3 - 4.5) #output will be -1.5
+print(3 * 4.5) #output will be 13.5
+print(3 / 4.5) #output will be 0.6666666666666666
+print(10 % 3) #output will be 1, because the reminder is 1
+
+print(3 * 4 + 5) #output will be 17
+print(3 * (4 + 5)) #output will be 27
+
+my_num = 5
+print(my_num) #output will be 5
+print(str(my_num)) #output will be 5, but it is converted into a string
+print(str(my_num) + " My favorite number")
+
+my_num = -5
+print(abs(my_num)) #output will be absolute value (5).
+
+print(pow(3,2)) #output will be 9 (3^2)
+print(max(4,6)) #output will be 6, because maximum value is 6.
+print(min(4,6)) #output will be 4, because minimum value is 4.
+
+print(round(3.2)) #output will be 3, because 3.2 is nearest with 3
+print(round(3.7)) #output will be 4, because 3.7 is nearest with 4
+
+from math import * #if we are using import math...
+print(floor(3.2)) #output will be 3, because 3.2 is nearest with 3
+print(ceil(3.7)) #output will be 4, because 3.7 is nearest with 4
+
+print(sqrt(36)) #output will be 6.0
