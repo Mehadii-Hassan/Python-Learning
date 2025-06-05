a541cc9af618f10b26ce6d937080469b9915ce05
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-05 23:50:04 +0600

    for loop

---
+++ for loop.py
@@ -0,0 +1,9 @@
+friends = ["Mainul", "Rafi", "Sakib"]
+for friend in friends:
+    print(friend) #output - Mainul Rafi Sakib
+    
+for name in range(10):
+    print(name) #output - 0-9 without 10
+
+for name in range(3, 10):
+    print(name) #output - 3-9 without 10
