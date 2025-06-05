55cb8ecfbbed640a7115c55cb194a675d6796e64
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-05 09:23:13 +0600

    basic lists

---
+++ lists.py
@@ -0,0 +1,14 @@
+friends = ["Khalid", "Mainul", "Rafi", "Talha", "Sakib"] #this is a list of my friends
+print(friends) #output will be ['Khalid', 'Mainul', 'Rafi', 'Talha', 'Sakib']
+
+print(friends[0]) #output will be Khalid
+print(friends[2]) #output will be Rafi
+
+print(friends[-1]) #output will be Rafi
+print(friends[-2]) #output will be Mainul
+
+print(friends[1:]) #output will be ['Mainul', 'Rafi', 'Talha', 'Sakib']
+print(friends[1:3]) #output will be ['Mainul', 'Rafi']
+
+friends[1] = "Sojib"
+print(friends[1]) #output will be Sojib
