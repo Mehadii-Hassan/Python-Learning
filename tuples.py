3ce92ee101568c2be32201ea814ad7c08b1fab59
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-05 10:22:18 +0600

    tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+coordinates = (4, 5)
+print(coordinates) #output will be (4, 5)
+print(coordinates[0]) #output will be 4
+print(coordinates[1]) #output will be 5
+
+coordinates = [(4, 5), (6, 7), (80, 90)]
+coordinates[1] = (10, 22)
+print(coordinates)
+
+#Note : Tuple item can't be change, but if I use list in tuple, than we can change the item.
