105a5f79515ac816317d1862dda09cb1822b3121
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-06 23:36:27 +0600

    comments

---
+++ comments.py
@@ -0,0 +1,17 @@
+#This is single line comment
+
+"""
+this is
+multiple
+line comment
+(using double cautation)
+"""
+
+'''
+this is 
+also multiple
+line comment
+(using single cautation)
+'''
+
+print("Comments are fun.")
