7f332631dfe26f8e5251215b5bb91652346535c1
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-06 23:20:00 +0600

    build a translator

---
+++ build a translator.py
@@ -0,0 +1,15 @@
+#all vowels convert ingo  -> g
+#example : dog -> dgg, cat -> cgt
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+print(translate(input("Enter a phrase: ")))
