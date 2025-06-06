175f8d6aff7714ac6288cfbca25a11cfa27b91b9
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-07 00:57:36 +0600

    writing files

---
+++ writing to files.py
@@ -0,0 +1,9 @@
+employee_file = open("employees.txt", "w") #a means append (adding data in the last)
+employee_file.write("\nAyesha - Admission Candidate") #only this line is showing in txt file, because this is write operation
+employee_file.close()
+
+"""
+we can create new txt file using writing operation.
+in line one we change the file name : employees1.txt
+or index.html - for this in line two we use ("<p>Mehedi<p>")
+"""
