b4f463bb46becb6bd38084881a043f6c8b38afdc
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-08 12:16:28 +0600

    Variable (strings, integers, float, boolean)

---
+++ python learning 2.py
@@ -0,0 +1,40 @@
+#Strings
+first_name = "Mehedi"
+food = "Kacchi"
+email = "mehadi.cse01@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}\n")
+
+#Integers
+age = 21
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students\n")
+
+#Float
+price = 10.99
+cgpa = 3.2
+distance = 5.5
+
+print(f"The price is ${price}")
+print(f"Your cgpa is: {cgpa}")
+print(f"You ran {distance}km\n")
+
+#Boolean
+is_student = True
+is_online = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
