312214d82ce09eb0de08c4447247430326c055ad
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-08 12:27:04 +0600

    typecasting

---
+++ python learning 3.py
@@ -0,0 +1,17 @@
+#Typecasting = the process of converting a variable from one data type to another.
+# str(), int(), float(), bool()
+
+name = "Mehedi"
+age = 22
+cgpa = 3.2
+is_student = True
+
+cgpa = int(cgpa)
+age = float(age)
+age = str(age)
+name = bool(name)
+
+print(cgpa) #output - 3
+print(age) #output - 22.0
+print(age) #output - 22.0 (this is string)
+print(name) #output - True
