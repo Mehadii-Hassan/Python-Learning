b4186946d71287324fa7fac134b76c235335746f
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-08 14:24:46 +0600

    madlibs game

---
+++ python learning 7.py
@@ -0,0 +1,11 @@
+adjective1 = input("Enter an adjective (description): ")
+noun = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an adjective (description): ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a {noun}")
+print(f"{noun} was {adjective2} and {verb}")
+print(f"I was {adjective3}!")
+
