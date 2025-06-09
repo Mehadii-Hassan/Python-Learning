578e83185d528f9f335b056f6524d26e890b2138
Mehadi Hassan <mehadi.cse01@gmail.com>
2025-06-09 21:39:36 +0600

    2D set (outer row is tuple but inner row is set)

---
+++ python learning 28.py
@@ -0,0 +1,18 @@
+#example 1
+fruits = ({"apple", "orange", "banana", "coconut"})
+vegetables = ({"celery", "carrots", "potatoes"})
+meats = ({"chicken", "fish", "turkey"})
+
+dinner_table = (fruits, vegetables, meats)
+
+print(dinner_table) #output - ({'apple', 'orange', 'banana', 'coconut'}, {'celery', 'carrots', 'potatoes'}, {'fish', 'chicken', 'turkey'})
+
+#example 2
+lunch_table = ({"apple", "orange", "banana", "coconut"},
+               {"celery", "carrots", "potatoes"},
+               {"chicken", "fish", "turkey"})
+
+for collection in lunch_table:
+    for food in collection:
+        print(food, end=" ")
+    print()
